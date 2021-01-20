79f292ae065c9cd6d1c467a1f2d32597e7f0799e
KeremAtak <kerem.atak@hotmail.com>
2021-01-20 04:09:30 +0200

    minor test refactor

--- test/clj/dev_academy_2021/test/routes/names.clj
+++ test/clj/dev_academy_2021/test/routes/names.clj
@@ -1,7 +1,7 @@
 (ns dev-academy-2021.test.routes.names
   (:require [clojure.test :refer :all]
             [dev-academy-2021.handler :refer [app]]
-            [dev-academy-2021.test.routes.names-helper :refer [names-sorted-by-alphabet names-sorted-by-popularity]]
+            [dev-academy-2021.test.routes.names-helper :as helper]
             [muuntaja.core :as m]
             [ring.mock.request :refer [request]]))
 
@@ -9,8 +9,8 @@
   (testing "/api/names/alphabetical"
     (let [response ((app) (-> (request :get "/api/names/alphabetical")))]
       (is (= 200 (:status response)))
-      (is (= names-sorted-by-alphabet (m/decode-response-body response)))))
+      (is (= helper/names-sorted-by-alphabet (m/decode-response-body response)))))
   (testing "/api/names/popularity"
     (let [response ((app) (-> (request :get "/api/names/popularity")))]
       (is (= 200 (:status response)))
-      (is (= names-sorted-by-popularity (m/decode-response-body response))))))
+      (is (= helper/names-sorted-by-popularity (m/decode-response-body response))))))
